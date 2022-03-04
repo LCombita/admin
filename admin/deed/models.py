42c3ae4b978a3311dee2943714184774157ecffb
Lubian Combita <puertadiego@gmail.com>
2022-03-03 22:58:08 -0500

    se crear las app stage y project, se escribe el codigo para sus modelos

--- admin/deed/models.py
+++ admin/deed/models.py
@@ -1,7 +1,6 @@
-from turtle import update
 from django.db import models
-from django.utils.timezone import now
 from registration.models import User
+from project.models import Proyecto
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
@@ -16,45 +15,6 @@ class ActoJuridico(models.Model):
     def __str__(self):
         return self.nombre_acto
 
-
-class Cliente(models.Model):
-    nombre_cliente = models.CharField(max_length=100, unique=True, verbose_name='Cliente')
-
-    class Meta:
-        verbose_name = 'Cliente'
-        verbose_name_plural = 'Clientes'
-
-    def __str__(self):
-        return self.nombre_cliente
-
-
-class Proyecto(models.Model):
-    cliente = models.ForeignKey(
-        Cliente, on_delete=models.CASCADE, db_index=True, verbose_name='Cliente')
-    tramitador = models.ManyToManyField(
-        User, db_index=True, related_name='tramitador', verbose_name='Tramitador')
-    nombre_proyecto = models.CharField(max_length=100, unique=True, verbose_name='Proyecto')
-
-    class Meta:
-        verbose_name = 'Proyecto'
-        verbose_name_plural = 'Proyectos'
-
-    def __str__(self):
-        return self.nombre_proyecto
-
-#funciones para reparto
-def HojaRuta():
-        """retorna el id para mostrarlo en el formulario como hoja de ruta"""
-        return str(Reparto.id)
-
-def AnioEscritura(self):
-        """concatena el año de la fecha de escritura con el número de la escritura"""
-        if self.fecha_escritura:
-            a = self.fecha_escritura.strftime('%Y')
-            ae = a + '-' + str(self.escritura)
-            return ae
-        else:
-            return None
         
 class Reparto(models.Model):
     id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False)
@@ -123,85 +83,6 @@ class OtorganteReparto(models.Model):
     class Meta:
         verbose_name = 'Otorgante'
         verbose_name_plural = 'Otorgantes'
-
-
-class Etapa(models.Model):
-    nombre_etapa = models.CharField(
-        max_length=150, verbose_name='Etapa')
-    activo = models.BooleanField(
-        default=True, verbose_name='Activa')
-
-    class Meta:
-        verbose_name = 'Etapa'
-        verbose_name_plural = 'Etapas'
-
-    def __ster__(self):
-        return self.nombre_etapa
-
-
-class RepartoEtapa(models.Model):
-    reparto = models.ForeignKey(
-        Reparto, on_delete=models.CASCADE, db_index=True, verbose_name='Reparto')
-    etapa = models.ForeignKey(
-        Etapa, on_delete=models.CASCADE, db_index=True, verbose_name='Etapa')
-    fecha_inicio = models.DateField(
-        verbose_name='Fecha Inicio', help_text="Introduzca la fecha en formato: <em>YYYY-MM-DD</em>.")
-    fecha_final = models.DateField(
-        verbose_name='Fecha Final', help_text="Introduzca la fecha en formato: <em>YYYY-MM-DD</em>.")
-
-    class Meta:
-        verbose_name = 'Etapa Hoja Ruta'
-        verbose_name_plural = 'Etapas Hoja Ruta'
-        ordering = ['-id']
-
-
-class ObservacionEtapa(models.Model):
-    reparto_etapa = models.ForeignKey(
-        RepartoEtapa, on_delete=models.CASCADE, db_index=True, verbose_name='Reparto - Etapa')
-    observacion = models.CharField(
-        max_length=150, verbose_name='Observación')
-
-    class Meta:
-        verbose_name = 'Observación'
-        verbose_name_plural = 'Observaciones'
-
-    def __str__(self):
-        return self.observacion
-
-
-class Revision(models.Model):
-    reparto_etapa = models.ForeignKey(
-        RepartoEtapa, on_delete=models.CASCADE, db_index=True, verbose_name='Reparto - Etapa')
-    fecha_revision = models.DateField(
-        verbose_name='Fecha Final', help_text="Introduzca la fecha en formato: <em>YYYY-MM-DD</em>.")
-    reproceso = models.BooleanField(
-        default=False, verbose_name='Hubo Reproceso')
-    descripcion = models.CharField(
-        max_length=200, verbose_name='Descripción')
-
-    class Meta:
-        verbose_name = 'Revisión'
-        verbose_name_plural = 'Revisiones'
-
-    def __str__(self):
-        return self.descripcion
-
-
-class Impuesto(models.Model):
-    reparto_etapa = models.ForeignKey(
-        RepartoEtapa, on_delete=models.CASCADE, db_index=True, verbose_name='Reparto - Etapa')
-    boleta_rentas = models.CharField(
-        max_length=20, verbose_name='Boleta Rentas')
-    fecha_boleta_rentas = models.DateField(
-        verbose_name='Fecha Boleta Rentas', help_text="Introduzca la fecha en formato: <em>YYYY-MM-DD</em>.")
-    boleta_registro = models.CharField(
-        max_length=20, verbose_name='Boleta Registro')
-    fecha_boleta_registro = models.DateField(
-        verbose_name='Fecha Boleta Registro', help_text="Introduzca la fecha en formato: <em>YYYY-MM-DD</em>.")
-
-    class Meta:
-        verbose_name = 'Impuesto'
-        verbose_name_plural = 'Impuestos'
 
 
 #SEÑALES
---
+++ admin/stage/models.py
@@ -0,0 +1,81 @@
+from django.db import models
+from deed.models import Reparto
+
+
+class Etapa(models.Model):
+    nombre_etapa = models.CharField(
+        max_length=150, verbose_name='Etapa')
+    activo = models.BooleanField(
+        default=True, verbose_name='Activa')
+
+    class Meta:
+        verbose_name = 'Etapa'
+        verbose_name_plural = 'Etapas'
+
+    def __ster__(self):
+        return self.nombre_etapa
+
+
+class RepartoEtapa(models.Model):
+    reparto = models.ForeignKey(
+        Reparto, on_delete=models.CASCADE, db_index=True, verbose_name='Reparto')
+    etapa = models.ForeignKey(
+        Etapa, on_delete=models.CASCADE, db_index=True, verbose_name='Etapa')
+    fecha_inicio = models.DateField(
+        verbose_name='Fecha Inicio', help_text="Introduzca la fecha en formato: <em>YYYY-MM-DD</em>.")
+    fecha_final = models.DateField(
+        verbose_name='Fecha Final', help_text="Introduzca la fecha en formato: <em>YYYY-MM-DD</em>.")
+
+    class Meta:
+        verbose_name = 'Etapa Hoja Ruta'
+        verbose_name_plural = 'Etapas Hoja Ruta'
+        ordering = ['-id']
+
+
+class ObservacionEtapa(models.Model):
+    reparto_etapa = models.ForeignKey(
+        RepartoEtapa, on_delete=models.CASCADE, db_index=True, verbose_name='Reparto - Etapa')
+    observacion = models.CharField(
+        max_length=150, verbose_name='Observación')
+
+    class Meta:
+        verbose_name = 'Observación'
+        verbose_name_plural = 'Observaciones'
+
+    def __str__(self):
+        return self.observacion
+
+
+class Revision(models.Model):
+    reparto_etapa = models.ForeignKey(
+        RepartoEtapa, on_delete=models.CASCADE, db_index=True, verbose_name='Reparto - Etapa')
+    fecha_revision = models.DateField(
+        verbose_name='Fecha Final', help_text="Introduzca la fecha en formato: <em>YYYY-MM-DD</em>.")
+    reproceso = models.BooleanField(
+        default=False, verbose_name='Hubo Reproceso')
+    descripcion = models.CharField(
+        max_length=200, verbose_name='Descripción')
+
+    class Meta:
+        verbose_name = 'Revisión'
+        verbose_name_plural = 'Revisiones'
+
+    def __str__(self):
+        return self.descripcion
+
+
+class Impuesto(models.Model):
+    reparto_etapa = models.ForeignKey(
+        RepartoEtapa, on_delete=models.CASCADE, db_index=True, verbose_name='Reparto - Etapa')
+    boleta_rentas = models.CharField(
+        max_length=20, verbose_name='Boleta Rentas')
+    fecha_boleta_rentas = models.DateField(
+        verbose_name='Fecha Boleta Rentas', help_text="Introduzca la fecha en formato: <em>YYYY-MM-DD</em>.")
+    boleta_registro = models.CharField(
+        max_length=20, verbose_name='Boleta Registro')
+    fecha_boleta_registro = models.DateField(
+        verbose_name='Fecha Boleta Registro', help_text="Introduzca la fecha en formato: <em>YYYY-MM-DD</em>.")
+
+    class Meta:
+        verbose_name = 'Impuesto'
+        verbose_name_plural = 'Impuestos'
