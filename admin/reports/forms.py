b4918cc0f554d3d92c21613d0ce7d0d89d5ebf33
Lubian Combita <puertadiego@gmail.com>
2022-04-01 19:05:46 -0500

    finaliza repartos x otorgante

--- admin/reports/forms.py
+++ admin/reports/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class ReportRepartoXOtorganteForm(forms.Form):
 
-    identificacion = forms.CharField(label="Identificación", max_length=20,
+    identificacion = forms.CharField(label="Identificación", max_length=20, required=True,
         widget=forms.TextInput(attrs={'class':'form-control'}
         ))
--- admin/reports/views.py
+++ admin/reports/views.py
@@ -1,5 +1,6 @@
+from multiprocessing import context
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from .forms import ReportRepartoXOtorganteForm
 from deed.models import Reparto
 from registration.models import Grantor
@@ -9,18 +10,31 @@ class ReportRepartosXOtorganteView(TemplateView):
     
     template_name = 'reports/repartos_x_otorgante.html'
 
-    def get_context_data(self, request, **kwargs):
-        context = super().get_context_data(**kwargs)
-        #if self.request.method == "POST":
-            #id_form = ReportRepartoXOtorganteForm(data=self.request.POST)
-            #if id_form.is_valid():
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data()
         identificacion = request.POST.get('identificacion', '')
-        #identificacion = 1027845214
-        context['repartos'] = Reparto.objects.filter(otorgantereparto__otorgante__identification = identificacion)
-        context['otorgante'] = Grantor.objects.filter(identification = identificacion)
+        if identificacion:
+            g = Grantor.objects.filter(identification = identificacion).exists()
+            if g:    
+                context['repartos'] = Reparto.objects.filter(otorgantereparto__otorgante__identification = identificacion)
+                context['otorgante'] = Grantor.objects.get(identification = identificacion)
+        return super().render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context['form'] = ReportRepartoXOtorganteForm()
-        print ("desde vista: ", identificacion)
         return context
-        #else:
-         #   context['form'] = ReportRepartoXOtorganteForm()
-          #  return context
+
+
+class ReportRepartoProyectosListView(ListView):
+    """Gestiona la lista de hojas de ruta"""
+    model=Reparto
+"""
+    def get_queryset(self):
+        #Se crea un filtro para que se muestren solo los repartos activos
+        #correspondientes al tramitador logueado
+        #TODO: organizar el queryset para mostrar los reparto de los proyectos con tramitador logueado
+        qs = super().get_queryset()
+        return qs.filter(activo='True').filter(user=self.request.user).order_by('-id')
+
+"""
